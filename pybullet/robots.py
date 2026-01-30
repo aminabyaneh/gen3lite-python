7b1ff3988096f1781270921c704e7b1c4fdb9fda
David Meger <david.meger@gmail.com>
2026-01-30 10:51:14 -0500

    Updates to robots.

--- pybullet/robots.py
+++ pybullet/robots.py
@@ -11,7 +11,7 @@ class Gen3LiteArmController(object):
     This class provides methods to control the arm's joints and interact 
     with it surrounding environment through collision checking.
     """
-    def __init__(self, dt=1 / 50.0):
+    def __init__(self, dt=1 / 50.0, env_name="Free"):
         
         self.dt = dt
         pb.connect(pb.GUI,)
@@ -34,18 +34,15 @@ class Gen3LiteArmController(object):
         self.joint_ids = [pb.getJointInfo(self.__kinova_id, i) for i in range(self.__n_joints)]
         self.joint_ids = [j[0] for j in self.joint_ids if j[2] == pb.JOINT_REVOLUTE]
 
-        # Initialize to home position.
-        self.setToHome()
- 
         # This function creates obstacles and sets a reachable goal
-        self.createBalloonMaze()
+        self.createBalloonMaze(env_name)
 
         # Set the viewer's camera viewpoint
         pb.resetDebugVisualizerCamera(
             cameraDistance=1.5,
             cameraYaw=-120,
             cameraPitch=-10,
-            cameraTargetPosition=self.goalPosition
+            cameraTargetPosition=self.goal_position
         )
 
     def getRanges(self):
@@ -65,6 +62,18 @@ class Gen3LiteArmController(object):
     def setToHome(self):
         self.setJointAngles(self.__home_poses)
 
+    def fkine(self,angles):
+        # Record where we were (usually home, but just to be safe...)
+        curr = self.getCurrentJointAngles()
+        # Move the arm to the goal, specified in joint angles
+        self.setJointAngles(angles)
+        # Record the end effectors x,y,z position
+        state = pb.getLinkState(self.__kinova_id, self.END_EFFECTOR_INDEX)
+        # Move the arm back to where it began
+        self.setJointAngles(curr)
+
+        return state[0]
+
     def execPath(self,path):
         self.setJointAngles(self.__home_poses)
         
@@ -73,7 +82,34 @@ class Gen3LiteArmController(object):
             self.setJointAngles(p)
             time.sleep(0.25)
 
-    def createBalloonMaze(self):
+    def createBalloonMaze(self,env_name):
+        if env_name == "Hardest":
+            self.createHardestMaze()
+        elif env_name == "Easiest":
+            self.createEasiestMaze()
+        elif env_name == "Free":
+            self.createFreeMaze()
+
+    def createFreeMaze(self):
+        
+        # Initialize to home position.
+        self.setToHome()
+
+        # This is a hard-coded sensible goal to try to reach
+        self.goal_angles = [0.1026325022237283, -0.2931188624740633, 1.2717083400432991, 0.048794139164578594, 0.07744723004754135, -0.8437927483158898, -0.024709326684397483]
+
+        self.goal_position = self.fkine(self.goal_angles)
+
+        # Make a visual marker for the goal
+        goal_visual_id = pb.createVisualShape(pb.GEOM_BOX, 
+                                              halfExtents=[0.05,0.05,0.05],
+                                              rgbaColor=[0.0,0.0,1.0,0.5]
+                                              )
+        
+        pb.createMultiBody(baseMass=0, basePosition=self.goal_position, baseVisualShapeIndex=goal_visual_id )
+  
+    def createEasiestMaze(self):
+        self.createFreeMaze()
         balloon_collision_id = pb.createCollisionShape(pb.GEOM_SPHERE, radius=0.07)
         balloon_visual_id = pb.createVisualShape(pb.GEOM_SPHERE, radius=0.07,rgbaColor=[1.0,0.0,0.0,0.5])
 
@@ -84,31 +120,18 @@ class Gen3LiteArmController(object):
                 baseVisualShapeIndex=balloon_visual_id  
                 )
 
-        x = 0
+    def createHardestMaze(self):
+        self.createEasiestMaze()
+
         balloon_collision_id = pb.createCollisionShape(pb.GEOM_SPHERE, radius=0.15)
         balloon_visual_id = pb.createVisualShape(pb.GEOM_SPHERE, radius=0.15,rgbaColor=[1.0,0.0,0.0,0.5])
+
+        x = 0
         for y in [-0.3, 0.3]:
             for z in [0.2, 0.6]:
                 box_id = pb.createMultiBody(baseMass=0, basePosition=[x, y, z],baseCollisionShapeIndex=balloon_collision_id,
                 baseVisualShapeIndex=balloon_visual_id  
                 )        
-
-        # This is a hard-coded sensible goal to try to reach, in the middle of the four balloons.
-        self.goal = [0.1026325022237283, -0.2931188624740633, 1.2717083400432991, 0.048794139164578594, 0.07744723004754135, -0.8437927483158898, -0.024709326684397483]
-
-        # Record where we were (usually home, but just to be safe...)
-        curr = self.getCurrentJointAngles()
-        # Move the arm to the goal, specified in joint angles
-        self.setJointAngles(self.goal)
-        # Record the end effectors x,y,z position
-        goal_state = pb.getLinkState(self.__kinova_id, self.END_EFFECTOR_INDEX)
-        self.goalPosition = goal_state[0]
-        # Move the arm back to where it began
-        self.setJointAngles(curr)
-
-        # Now we can make a visual marker for the goal
-        goal_visual_id = pb.createVisualShape(pb.GEOM_BOX, halfExtents=[0.05,0.05,0.05],rgbaColor=[0.0,0.0,1.0,0.5])
-        box_id = pb.createMultiBody(baseMass=0, basePosition=goal_state[0], baseVisualShapeIndex=goal_visual_id )
     
     def visTreesAndPaths(self,trees,paths,rgbas_in):
         
@@ -137,16 +160,14 @@ class Gen3LiteArmController(object):
         for path in paths:
             for idx in range(len(path)):
                     
-                    self.setJointAngles(path[idx])
-                    parent_pos = pb.getLinkState(self.__kinova_id, self.END_EFFECTOR_INDEX)
+                    parent_pos = self.fkine(path[idx])
 
-                    node_sphere_id = pb.createMultiBody(baseMass=0, basePosition=parent_pos[0], baseVisualShapeIndex=point_vis_id )
+                    node_sphere_id = pb.createMultiBody(baseMass=0, basePosition=parent_pos, baseVisualShapeIndex=point_vis_id )
 
                     if idx+1 < len(path):
-                        self.setJointAngles(path[idx+1])
-                        child_pos = pb.getLinkState(self.__kinova_id, self.END_EFFECTOR_INDEX)
+                        child_pos = self.fkine(path[idx+1])
 
-                        pb.addUserDebugLine(lineFromXYZ=parent_pos[0],lineToXYZ=child_pos[0],lineColorRGB=[1.0, 1.0, 0.0],lineWidth=0.02,lifeTime=0)
+                        pb.addUserDebugLine(lineFromXYZ=parent_pos,lineToXYZ=child_pos,lineColorRGB=[1.0, 1.0, 0.0],lineWidth=0.02,lifeTime=0)
         
         self.setToHome()
         pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1)
